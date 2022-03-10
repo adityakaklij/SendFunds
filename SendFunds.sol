9a6db1cf6ee44d907bad4a157019948002ccbd1d
adityakaklij <90379168+adityakaklij@users.noreply.github.com>
2022-03-11 00:51:31 +0530

    Update SendFunds.sol

--- SendFunds.sol
+++ SendFunds.sol
@@ -1,1 +1,13 @@
+//SPDX-Licenses-Identifier:MIT
 
+pragma solidity ^0.8.0;
+
+contract SendFunds{
+
+  function sendViaCall(address payable _to) public payable {
+  
+        // Call returns a boolean value indicating success or failure.
+        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+    }
+}
